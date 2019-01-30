3aa6d6ba1fb7080e587c176d877f319786ff3f6b
Darkanel <ejgameror@gmail.com>
2019-01-30 15:18:28 -0400

    Reporte Controller actualizacion

--- app/Http/Controllers/ReporteController.php
+++ app/Http/Controllers/ReporteController.php
@@ -18,34 +18,84 @@ class ReporteController extends Controller
 
     public function generar()
     {
-        $articulos=DB::table('articulo as a')
-            ->join('categoria as c','a.idcategoria','=','c.idcategoria')
-            ->select('a.idarticulo','a.nombre','a.codigo','a.stock','a.costo','c.nombre as categoria','a.estado')
+        $personas=DB::table('persona')
+            ->where('tipo_persona','=','Proveedor')
             ->get();
+        dd($personas);
         $view = \View::make('pdf.reporte',compact('articulos'))->render();
         $pdf = \App::make('dompdf.wrapper');
         $pdf->loadHTML($view); 
         return $pdf->stream('informe'.'.pdf');
     }
-
-  /*  public function ReporteCategoria()
-    {
-        $categorias=DB::table('categoria')
-        	->get();
-    }*/
-
 
     public function ReporteArticulo()
     {
         $articulos=DB::table('articulo as a')
             ->join('categoria as c','a.idcategoria','=','c.idcategoria')
-            ->select('a.idarticulo','a.nombre','a.codigo','a.stock','a.costo','c.nombre as categoria','a.estado')
+            ->select('a.idarticulo','a.nombre','a.codigo','','a.costo','c.nombre as categoria','a.estado',DB::raw('sum(a.stock*di.precio_compra) as total'))
             ->get();
-        $view = \View::make('pdf.reporte',compact('articulos'))->render();
+        $view = \View::make('pdf.reportearticulo',compact('articulos'))->render();
         $pdf = \App::make('dompdf.wrapper');
         $pdf->loadHTML($view); 
         return $pdf->stream('informe'.'.pdf');
 
     }
+
+    public function ReporteClientes()
+    {
+        $personas=DB::table('persona')
+            ->where('tipo_persona','=','Cliente')
+            ->get();
+        $view = \View::make('pdf.reportecliente',compact('personas'))->render();
+        $pdf = \App::make('dompdf.wrapper');
+        $pdf->loadHTML($view); 
+        return $pdf->stream('informe'.'.pdf');
+    }
+
+    public function ReporteProveedor()
+    {
+        $personas=DB::table('persona')
+            ->where('tipo_persona','=','Proveedor')
+            ->get();
+        $view = \View::make('pdf.reporteproveedor',compact('personas'))->render();
+        $pdf = \App::make('dompdf.wrapper');
+        $pdf->loadHTML($view); 
+        return $pdf->stream('informe'.'.pdf');
+    }
+
+    public function ReporteIngreso()
+    {
+        $ingresos=DB::table('ingreso as i')
+            ->join('persona as p','i.idproveedor','=','p.idpersona')
+            ->join('detalle_ingreso as di','i.idingreso','=','di.idingreso')
+            ->select('i.idingreso','i.fecha_hora','p.nombre','i.tipo_comprobante','i.serie_comprobante','i.num_comprobante','i.estado',DB::raw('sum(di.cantidad*di.precio_compra) as total'))
+            ->get();
+        $view = \View::make('pdf.reporteproveedor',compact('personas'))->render();
+        $pdf = \App::make('dompdf.wrapper');
+        $pdf->loadHTML($view); 
+        return $pdf->stream('informe'.'.pdf');
+    }
+
+    public function ReporteIngresoID()
+    {
+        
+    }
+
+    public function ReporteVenta()
+    {
+        
+    }
+
+    public function ReporteVentaID()
+    {
+        
+    }
+
+    public function ReporteUsuario()
+    {
+        
+    }
+
+    
 
 }
