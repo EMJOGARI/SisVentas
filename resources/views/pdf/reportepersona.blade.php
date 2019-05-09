d95e486e1a4c31139bf743a1b2aeda1453ec106b
darkanel <ejgameror@gmail.com>
2019-05-09 01:50:23 -0400

    Funcion de cero a los Id

--- resources/views/pdf/reportepersona.blade.php
+++ resources/views/pdf/reportepersona.blade.php
@@ -14,7 +14,7 @@
 		</tr>		
 		@foreach ($personas as $per)
 			<tr>												
-				<td style="text-align: center;">{{ $per->idpersona }}</td>
+				<td style="text-align: center;">{{ str_pad($per->idpersona, 3, "0", STR_PAD_LEFT) }}</td>
 				<td>{{ $per->nombre }}</td>
 				<td>{{ $per->tipo_documento.' - '.$per->num_documento }}</td>
 				<td>{{ $per->telefono }}</td>
